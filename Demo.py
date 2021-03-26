40bad01c85b07cc9ba0f345dd825b599c2841f14
Shanice2021 <109021487@live.asia.edu.tw>
2021-03-26 16:01:33 +0800

    class

--- Demo.py
+++ Demo.py
@@ -1,13 +1,22 @@
 import requests as req
 from bs4 import BeautifulSoup
 r=req.get(
-    "http://120.108.116.237/~jackjow/pubList.php")
+    "http://isrc.ccs.asia.edu.tw/www/myjournal/myjournal.htm")
 r.encoding= "utf8"
 if r.status_code==200:
-#    print(r.text)
+    #print(r.text)
     soup=BeautifulSoup(r.text,"lxml")
-#    print(soup)
-    result1=soup.find_all("li")
-    print(result1)
+    #print(soup)
+    result1=soup.find_all("p")
+    fp=open("out2.txt","w",encoding="utf8")
+    #print(result1)
+    for val in result1:
+        #print(val.text)
+        text2=val.text.replace('\n','')
+        #print(text2)
+        text3=text2.replace('  ','')
+        print(text3)
+        fp.write(text3+'\n')
+    fp.close()
 else:
     print("no page")
